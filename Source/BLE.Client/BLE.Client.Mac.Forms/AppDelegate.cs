a6c76ab773012ab1a876e2d463918bfcf5fc0644
Felix.x <flyingxu@gmail.com>
2017-09-01 15:03:58 +0800

    mac support

---
+++ Source/BLE.Client/BLE.Client.Mac.Forms/AppDelegate.cs
@@ -0,0 +1,39 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace BLE.Client.Mac.Forms
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : FormsApplicationDelegate
+	{
+		NSWindow _window;
+
+		public AppDelegate()
+		{
+			var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+			var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+			_window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+			_window.Title = "Xamarin.Forms on Mac!";
+					_window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+		}
+
+		public override NSWindow MainWindow
+		{
+		get { return _window; }
+		}
+
+		public override void DidFinishLaunching(NSNotification notification)
+		{
+			Xamarin.Forms.Forms.Init();
+			LoadApplication(new BleMvxFormsApp());
+			base.DidFinishLaunching(notification);
+		}
+
+		public override void WillTerminate(NSNotification notification)
+		{
+			// Insert code here to tear down your application
+		}
+	}
+}
---
+++ Source/BLE.Client/BLE.Client.Mac.Forms/Main.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace BLE.Client.Mac.Forms
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new AppDelegate();
+			NSApplication.Main(args);
+		}
+	}
+}
